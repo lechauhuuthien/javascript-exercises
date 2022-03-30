452b7ddfd7794b476f9cdac6ae2fa1594b37875a
annguyen1511 <annguyen1511>
2022-03-30 09:35:47 +0700

    practice dom

---
+++ Dom/ChangeBackgroundBox/index.js
@@ -0,0 +1,16 @@
+
+/* <-------  Change background by button  -------> */
+
+
+
+const changeBg = (bg) => {
+    /* <-------  Here your code  -------> */
+}
+
+
+/* <-------  Code sample  -------> */
+
+// const changeBg = (bg) => {
+//     $box = document.querySelector(".box");
+//     $box.style.backgroundColor = bg;
+// }
---
+++ Dom/GetValueForm/index.js
@@ -0,0 +1,24 @@
+
+/* <-------  Write a JavaScript function to get the values of First and Last name of the following form.  -------> */
+
+const $firstName = document.getElementById("firstName");
+const $lastName = document.getElementById("lastName");
+
+
+const getFormValue = () => {
+    /* <-------  Here your code  -------> */
+}
+
+
+/* <-------  Code Sample  -------> */
+
+
+// const getFormValue = () => {
+//     var x = document.getElementById("form1");
+//     for (var i = 0; i < x.length; i++) {
+//         if (x.elements[i].value != 'Submit') {
+//             console.log(x.elements[i].value);
+//         }
+//     }
+// }
+
